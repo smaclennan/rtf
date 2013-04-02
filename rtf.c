259c1a257e1f28dc4e929192c6f4ffc20418e22e
Sean MacLennan <git@seanm.ca>
2013-04-01 22:58:40 -0400

    rtf(tm) now has a trivial whitelist and runs the mail through bogofilter.
    Very very unconfigurable.

--- rtf.c
+++ rtf.c
@@ -37,6 +37,12 @@
 
 static char *flist[] = { "vigara", "viagra", "data entry", NULL };
 
+static char *wlist[] = { "enbridge", "linkedin", NULL };
+
+#define BOGOFILTER "bogofilter -u"
+
+static char header[8096];
+
 
 static int filter(char *line)
 {
@@ -49,12 +55,42 @@ static int filter(char *line)
 	return 0;
 }
 
+static int whitelist(char *line)
+{
+	int i;
+
+	for (i = 0; wlist[i]; ++i)
+		if (strcasestr(line, wlist[i]))
+			return 1; /* whitelisted */
+
+	return 0;
+}
+
+static int run_bogofilter(void)
+{
+	FILE *pfp = popen(BOGOFILTER, "w");
+	if (!pfp) {
+		syslog(LOG_WARNING, "bogofilter failed to start");
+		return 0;
+	}
+
+	fputs(header, pfp);
+
+	char line[1024];
+	while (fgets(line, sizeof(line), stdin))
+		fputs(line, pfp);
+
+	return WEXITSTATUS(pclose(pfp));
+
+}
+
 int main(int argc, char *argv[])
 {
-	char line[1024];
+	char *line = header;
+	int len = sizeof(header);
 	int saw_from = 0, saw_date = 0;
 
-	while (fgets(line, sizeof(line), stdin))
+	while (fgets(line, len, stdin)) {
 		if (*line == '\n')
 			break; /* end of header */
 		else if (strncmp(line, "From:", 5) == 0) {
@@ -62,16 +98,27 @@ int main(int argc, char *argv[])
 				return 0; /* spam */
 			else
 				saw_from = 1;
+			if (whitelist(line))
+				return 1; /* don't redirect, don't run though bogofilter */
 		} else if (strncmp(line, "Subject:", 8) == 0) {
 			if (filter(line))
 				return 0; /* spam */
 		} else if (strncmp(line, "Date:", 5) == 0)
 			saw_date = 1;
 
+		int n = strlen(line);
+		line += n;
+		len -= n;
+	}
+
 	if (saw_from == 0 || saw_date == 0)
 		return 0; /* spam */
 
+#if 0
 	return 1; /* don't redirect */
+#else
+	return run_bogofilter();
+#endif
 }
 
 /*
