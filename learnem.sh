0f80b7a82a12cd0c591f480a1451ee02d4dd598d
Sean MacLennan <seanm@seanm.ca>
2015-10-11 20:35:45 -0400

    The ls commands didn't work well becasue of the special chars in the file names.

--- learnem.sh
+++ learnem.sh
@@ -2,18 +2,18 @@
 
 for d in /home/*; do
     [ -d $d/.bogofilter ] || continue
-
+	
     # Process spam
-    for f in `ls $d/Maildir/.LearnSPAM/new` `ls $d/Maildir/.LearnSPAM/cur`; do
-	echo spam $f # SAM DBG
-	bogofilter -Ns -I $f -d $d/.bogofilter
-	mv $f ../../.Spam/new
-    done
+	bogofilter -Ns -d $d/.bogofilter -B $d/Maildir/.LearnSPAM
+	if [ -n "`ls $d/Maildir/.LearnSPAM/cur`" ]; then
+		mv $d/Maildir/.LearnSPAM/cur/* $d/Maildir/.Spam/cur
+	fi
+	if [ -n "`ls $d/Maildir/.LearnSPAM/new`" ]; then
+		mv $d/Maildir/.LearnSPAM/new/* $d/Maildir/.Spam/new
+	fi
 
     # Process ham
-    for f in `ls $d/Maildir/.Ham/new` `ls $d/Maildir/.Ham/cur`; do
-	echo ham $f # SAM DBG
-	bogofilter -Sn -I $f -d $d/.bogofilter
-	rm $f
-    done
+	bogofilter -Sn -d $d/.bogofilter -B $d/Maildir/.Ham
+	rm -f $d/Maildir/.Ham/cur/*
+	rm -f $d/Maildir/.Ham/new/*
 done
