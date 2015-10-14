a9455e94d781f89499a97ff6774abf4dea53ee86
Sean MacLennan <seanm@seanm.ca>
2015-10-13 21:13:26 -0400

    Switch from a loop through /home to one user. This means I can run it non-privileged.

--- learnem.sh
+++ learnem.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/sh -e
 
-for d in /home/*; do
-    [ -d $d/.bogofilter ] || continue
-	
-    # Process spam
-	bogofilter -Ns -d $d/.bogofilter -B $d/Maildir/.LearnSPAM
-	if [ -n "`ls $d/Maildir/.LearnSPAM/cur`" ]; then
-		mv $d/Maildir/.LearnSPAM/cur/* $d/Maildir/.Spam/cur
-	fi
-	if [ -n "`ls $d/Maildir/.LearnSPAM/new`" ]; then
-		mv $d/Maildir/.LearnSPAM/new/* $d/Maildir/.Spam/new
-	fi
+[ -d $HOME/.bogofilter ] || exit 1
+cd $HOME/Maildir || exit 1
 
-    # Process ham
-	bogofilter -Sn -d $d/.bogofilter -B $d/Maildir/.Ham
-	rm -f $d/Maildir/.Ham/cur/*
-	rm -f $d/Maildir/.Ham/new/*
-done
+# Process spam
+bogofilter -Ns -d $HOME/.bogofilter -e -B .LearnSPAM
+if [ -n "`ls .LearnSPAM/cur`" ]; then
+    mv .LearnSPAM/cur/* .Spam/cur
+fi
+if [ -n "`ls .LearnSPAM/new`" ]; then
+    mv .LearnSPAM/new/* .Spam/new
+fi
+
+# Process ham
+bogofilter -Sn -d $HOME/.bogofilter -e -B .Ham
+rm -f .Ham/cur/*
+rm -f .Ham/new/*
+
+exit 0
