e07d5c46dbab930996506e9a5f878a20e7e10777
Sean MacLennan <seanm@seanm.ca>
2016-03-26 13:33:53 -0400

    Added date ranges to rtfsort.

--- rtfsort.c
+++ rtfsort.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 #include <errno.h>
+#include <time.h>
 
 
 static struct list {
@@ -13,6 +14,9 @@ static struct list {
 	const char *subject;
 	struct list *prev, *next;
 } *head, *tail;
+
+static int verbose;
+static time_t start = (time_t)-1, end = (time_t)-1;
 
 
 static void add_list(const char *fname, const char *subject)
@@ -70,6 +74,90 @@ void dump_list(void)
 		puts(l->subject);
 }
 
+static int get_month(char *mstr)
+{
+	static char *months[] = {
+		"jan", "feb", "mar", "apr", "may", "jun",
+		"jul", "aug", "sep", "oct", "nov", "dec"
+	};
+	int i;
+
+	for (i = 0; i < 12; ++i)
+		if (strncasecmp(mstr, months[i], 3) == 0)
+			return i;
+
+	printf("Invalid month '%s'\n", mstr);
+	exit(1);
+}
+
+static time_t set_date(char *arg, int end)
+{
+	time_t now = time(NULL);
+	struct tm *tm = localtime(&now);
+	char *p;
+
+	p = strtok(arg, ", ");
+	if (isalpha(*p)) {
+		tm->tm_mon = get_month(p);
+		if ((p = strtok(NULL, ", ")))
+			tm->tm_mday = strtol(p, NULL, 10);
+	} else
+		tm->tm_mday = strtol(p, NULL, 10);
+	if ((p = strtok(NULL, ", "))) {
+		long year = strtol(p, NULL, 10);
+		if (year < 1900) {
+			printf("Invalid year %ld\n", year);
+			exit(1);
+		}
+		tm->tm_year = year - 1900;
+	}
+
+	/* reset to midnight */
+	tm->tm_sec = tm->tm_min = tm->tm_hour = 0;
+	tm->tm_isdst = -1;
+	if (end)
+		++tm->tm_mday;
+	return mktime(tm);
+}
+
+static void set_dates(char *arg)
+{
+	char *p;
+
+	if ((p = strchr(arg, ':'))) {
+		*p++ = 0;
+		end = set_date(p, 1);
+	}
+	if (*arg)
+		start = set_date(arg, 0);
+	if (start != (time_t)-1 && end != (time_t)-1)
+		if (start > end) {
+			time_t tmp = start;
+			start = end;
+			end = tmp;
+		}
+}
+
+static int date_in_range(char *fname)
+{
+	char *p;
+	time_t date = strtol(fname, &p, 10);
+	if (*p != '.') {
+		printf("Problems with date\n");
+		return 1;
+	}
+
+	if (start != (time_t)-1)
+		if (date < start)
+			return 0;
+
+	if (end != (time_t)-1)
+		if (date >= end)
+			return 0;
+
+	return 1;
+}
+
 int main(int argc, char *argv[])
 {
 	char line[80], fname[80], subject[80];
@@ -77,6 +165,17 @@ int main(int argc, char *argv[])
 	char from_me, learn, learn_flag;
 	unsigned total = 0, not_me = 0, from = 0, ignored = 0, real = 0, spam = 0;
 	unsigned learned = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "d:v")) != EOF)
+		switch (c) {
+		case 'd':
+			set_dates(optarg);
+			break;
+		case 'v':
+			++verbose;
+			break;
+		}
 
 	while (fgets(line, sizeof(line), stdin))
 		if (sscanf(line, "%s %c%c%c%c%c%c%c%c%c%c %[^\n]",
@@ -84,12 +183,14 @@ int main(int argc, char *argv[])
 				   &is_ignore, &is_spam, &from_me, &bogo_spam,
 				   &learn, &learn_flag,
 				   subject) == 12) {
+			if (!date_in_range(fname)) continue;
+			if (verbose)
+				fputs(line, stderr);
 			if (learn == 'L') {
+				++learned;
 				if (learn_flag == 'S')
-					if (check_list(fname)) {
+					if (check_list(fname))
 						--real;
-						++learned;
-					}
 			}
 			++total;
 			if (is_me == '-' && is_ignore == '-' && is_ham == '-')
@@ -112,11 +213,13 @@ int main(int argc, char *argv[])
 						  fname, &is_me, &saw_from, &saw_date, &is_ham,
 						  &is_ignore, &is_spam, &bogo_spam,
 						  &from_me, &learn, &learn_flag) == 11) {
+			if (!date_in_range(fname)) continue;
+			if (verbose)
+				fputs(line, stderr);
+			++learned;
 			if (learn_flag == 'S')
-				if (check_list(fname)) {
+				if (check_list(fname))
 					--real;
-					++learned;
-				}
 		} else
 			printf("PROBS: %s", line);
 
