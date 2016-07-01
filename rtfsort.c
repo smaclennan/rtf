7d65b21730df1a8774d070855bd0db025cbb9dbf
Sean MacLennan <seanm@seanm.ca>
2016-07-01 17:30:52 -0400

    Add -a option to count actions.

--- rtfsort.c
+++ rtfsort.c
@@ -18,6 +18,40 @@ static struct list {
 static int verbose;
 static time_t start = (time_t)-1, end = (time_t)-1;
 
+struct log_struct {
+	char fname[80];
+	char is_me;
+	char saw_from;
+	char saw_date;
+	char is_ham;
+	char is_ignore;
+	char is_spam;
+	char from_me;
+	char bogo_spam;
+	char learn;
+	char learn_flag;
+	char subject[80];
+};
+
+struct sort_counts {
+	/* both */
+	unsigned total;
+	unsigned ignored;
+	unsigned real;
+	unsigned spam;
+	unsigned learned;
+
+	/* only sort */
+	unsigned not_me;
+	unsigned from;
+
+	/* only for actions */
+	unsigned drop;
+	unsigned ham;
+	unsigned bogo;
+	unsigned bogo_total;
+	unsigned learned_ham;
+};
 
 static void add_list(const char *fname, const char *subject)
 {
@@ -158,17 +192,78 @@ static int date_in_range(char *fname)
 	return 1;
 }
 
+static void handle_line(struct log_struct *l, struct sort_counts *sc)
+{
+	if (l->learn == 'L') {
+		++sc->learned;
+		if (l->learn_flag == 'S') {
+			if (check_list(l->fname)) {
+				--sc->real;
+				++sc->spam;
+			} else
+				printf("Problems %s\n", l->fname);
+		} else if (l->learn_flag == 'H') {
+			--sc->spam;
+			++sc->real;
+		} else
+			printf("Problems learn flags %c\n", l->learn_flag);
+		return;
+	}
+
+	if (l->is_ignore == 'I')
+		++sc->ignored;
+	else if (l->is_ham == 'H') {
+		++sc->real;
+		add_list(l->fname, l->subject);
+	} else if (l->is_me == '-')
+		++sc->not_me;
+	else if (l->bogo_spam == '-' && l->from_me == '-') {
+		++sc->real;
+		add_list(l->fname, l->subject);
+	} else
+		++sc->spam;
+	if (l->from_me == 'f')
+		++sc->from;
+}
+
+static void handle_actions(struct log_struct *l, struct sort_counts *sc)
+{
+	if (l->learn == 'L') {
+		if (l->learn_flag == 'S')
+			++sc->learned;
+		else
+			++sc->learned_ham;
+	} else if (l->is_ignore == 'I')
+		++sc->ignored;
+	else if (l->is_ham == 'H')
+		++sc->ham;
+	else if (l->is_spam == 'A' || l->is_spam == 'Z')
+		++sc->drop;
+	else if (l->is_spam == 'S' ||
+			 l->saw_from == '-' || l->saw_date == '-' ||
+			 l->from_me == 'M' || l->is_me == '-')
+		++sc->spam;
+	else if (l->bogo_spam == 'B')
+		++sc->bogo;
+	else
+		++sc->real;
+
+	if (l->bogo_spam == 'B')
+		++sc->bogo_total;
+}
+
 int main(int argc, char *argv[])
 {
-	char line[80], fname[80], *subject;
-	char is_me, saw_from, saw_date, is_ham, is_ignore, is_spam, bogo_spam;
-	char from_me, learn, learn_flag;
-	unsigned total = 0, not_me = 0, from = 0, ignored = 0, real = 0, spam = 0;
-	unsigned learned = 0;
-	int c, n;
+	char line[80];
+	int c, n, do_actions = 0;
+	struct log_struct l;
+	struct sort_counts sc;
 
-	while ((c = getopt(argc, argv, "d:v")) != EOF)
+	while ((c = getopt(argc, argv, "ad:v")) != EOF)
 		switch (c) {
+		case 'a': /* count actions */
+			do_actions = 1;
+			break;
 		case 'd':
 			set_dates(optarg);
 			break;
@@ -177,60 +272,52 @@ int main(int argc, char *argv[])
 			break;
 		}
 
-	while (fgets(line, sizeof(line), stdin))
+	memset(&sc, 0, sizeof(sc));
+
+	while (fgets(line, sizeof(line), stdin)) {
 		if (sscanf(line, "%s %c%c%c%c%c%c%c%c%c%c%n",
-				   fname, &is_me, &saw_from, &saw_date, &is_ham,
-				   &is_ignore, &is_spam, &from_me, &bogo_spam,
-				   &learn, &learn_flag,
-				   &n) == 11) {
-			if (!date_in_range(fname)) continue;
-			++total;
+				   l.fname, &l.is_me, &l.saw_from, &l.saw_date,
+				   &l.is_ham, &l.is_ignore, &l.is_spam, &l.from_me,
+				   &l.bogo_spam, &l.learn, &l.learn_flag, &n) == 11) {
+			if (!date_in_range(l.fname)) continue;
+			++sc.total;
 			if (verbose)
 				fputs(line, stderr);
 
-			if (learn == 'L') {
-				++learned;
-				if (learn_flag == 'S') {
-					if (check_list(fname)) {
-						--real;
-						++spam;
-					} else
-						printf("Problems %s\n", fname);
-				} else if (learn_flag == 'H') {
-					--spam;
-					++real;
-				} else
-						printf("Problems learn flags %c\n", learn_flag);
-				continue;
-			}
+			if (line[n] == ' ')
+				++n;
+			snprintf(l.subject, sizeof(l.subject), "%s", line + n);
 
-			subject = line + n;
-			if (*subject == ' ') ++subject;
-
-			if (is_ignore == 'I')
-				++ignored;
-			else if (is_ham == 'H') {
-				++real;
-				add_list(fname, subject);
-			} else if (is_me == '-')
-				++not_me;
-			else if (bogo_spam == '-' && from_me == '-') {
-				++real;
-				add_list(fname, subject);
-			} else
-				++spam;
-			if (from_me == 'f')
-				++from;
+			if (do_actions)
+				handle_actions(&l, &sc);
+			else
+				handle_line(&l, &sc);
 		} else
 			printf("PROBS: %s", line);
+	}
 
-	if (not_me + ignored + real + learned + spam != total)
-		printf("Problems with total\n");
+	if (do_actions) {
+		printf("Ignored %u ham %u drop %u spam %u bogo %u real %u learned %u\n",
+			   sc.ignored, sc.ham, sc.drop, sc.spam, sc.bogo, sc.real, sc.learned);
 
-	// dump_list();
+		unsigned actual_spam = sc.spam + sc.drop + sc.bogo + sc.learned;
 
-	printf("Not me %u from me %u ignored %d real %u learned %u spam %u total %u\n",
-		   not_me, from, ignored, real, learned, spam, total);
+		printf("We caught %.0f%% bogofilter %.0f%% missed %.0f%%\n",
+			   (double)(sc.spam + sc.drop) * 100.0 / (double)actual_spam,
+			   (double)sc.bogo_total * 100.0 / (double)actual_spam,
+			   (double)sc.learned * 100.0 / (double)actual_spam);
+		printf("Spam was %.0f%% of all messages\n",
+			   (double)actual_spam * 100.0 / (double)sc.total);
+	} else {
+		if (sc.not_me + sc.ignored + sc.real + sc.learned + sc.spam != sc.total)
+			printf("Problems with total\n");
+
+		// dump_list();
+
+		printf("Not me %u from me %u ignored %d real %u learned %u spam %u total %u\n",
+			   sc.not_me, sc.from, sc.ignored, sc.real, sc.learned, sc.spam, sc.total);
+	}
+
 	return 0;
 }
 
