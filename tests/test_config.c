1db2ea6270ee0061b2402d75d9ed3ecd2f1e27cd
Sean MacLennan <seanm@seanm.ca>
2019-04-30 20:06:21 -0400

    Update the test_config

--- tests/test_config.c
+++ tests/test_config.c
@@ -8,11 +8,7 @@
 #include <errno.h>
 #include <assert.h>
 
-const char *home = "/home/test";
-int just_checking;
-int verbose;
-
-#define MAX_LINES 10
+#define MAX_LINES 20
 static char *lines[MAX_LINES];
 static int  curline;
 
@@ -35,10 +31,10 @@ static int my_fclose(FILE *fp) { return 0; }
 #undef fgets
 #undef fclose
 
-static void verify_list(struct entry *head, int start)
+static void verify_list(struct entry *head, int start, int end)
 {
 	struct entry *e = head;
-	for (int i = start; lines[i]; ++i) {
+	for (int i = start; i < end; ++i) {
 		assert(e);
 		char *p = strchr(lines[i], '=');
 		if (p) {
@@ -60,111 +56,75 @@ int main(int argc, char *argv[])
 	/* Add some entries */
 	lines[0] = "[global]";
 	lines[1] = "server=bogus";
-	lines[2] = "[folders]";
-	lines[3] = "root=father";
-	lines[4] = "[ignore]";
-	lines[5] = "everything";
+	lines[2] = "port=993";
+	lines[3] = "user=me";
+	lines[4] = "passwd=none of your business";
+	lines[5] = "[folders]";
+	lines[6] = "root=father";
+	lines[7] = "[whitelist]";
+	lines[8] = "everything";
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	assert(global != NULL);
-	assert(strcmp(global->str, "server") == 0);
-	assert(strcmp(global->folder, "bogus") == 0);
-	assert(global->next == NULL);
-
-	assert(folderlist != NULL);
-	assert(strcmp(folderlist->str, "root") == 0);
-	assert(strcmp(folderlist->folder, "father") == 0);
-	assert(folderlist->next == NULL);
-
-	verify_list(ignorelist, 5);
+	verify_list(global, 1, 5);
+	verify_list(folderlist, 6, 7);
+	verify_list(whitelist, 8, 9);
 
 	/* change the global and add some entries */
 	lines[1] = "server=good";
-	lines[6] = "nothing";
-	lines[7] = "meh";
-	lines[8] = "fred";
+	lines[9] = "nothing";
+	lines[10] = "meh";
+	lines[11] = "fred";
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	assert(global != NULL);
-	assert(strcmp(global->str, "server") == 0);
-	assert(strcmp(global->folder, "good") == 0);
-	assert(global->next == NULL);
-
-	assert(folderlist != NULL);
-	assert(strcmp(folderlist->str, "root") == 0);
-	assert(strcmp(folderlist->folder, "father") == 0);
-	assert(folderlist->next == NULL);
-
-	verify_list(ignorelist, 5);
+	verify_list(global, 1, 5);
+	verify_list(folderlist, 6, 7);
+	verify_list(whitelist, 8, 12);
 
 	/* delete a first entry */
-	for (int i = 5; i <= 8; ++i)
+	for (int i = 8; i <= 12; ++i)
 		lines[i] = lines[i + 1];
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	verify_list(ignorelist, 5);
+	verify_list(global, 1, 5);
+	verify_list(folderlist, 6, 7);
+	verify_list(whitelist, 8, 11);
 
 	/* delete a middle entry */
-	for (int i = 6; i <= 7; ++i)
+	for (int i = 9; i <= 10; ++i)
 		lines[i] = lines[i + 1];
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	verify_list(ignorelist, 5);
+	verify_list(global, 1, 5);
+	verify_list(folderlist, 6, 7);
+	verify_list(whitelist, 8, 10);
 
 	/* delete a last entry */
-	lines[6] = NULL;
+	lines[8] = NULL;
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	verify_list(ignorelist, 5);
+	verify_list(global, 1, 5);
+	verify_list(folderlist, 6, 7);
+	assert(whitelist == NULL);
 
-	/* delete the last entry */
-	lines[5] = NULL;
-
-	curline = 0;
-	assert(read_config() == 0);
-
-	verify_list(ignorelist, 5);
-
-	/* delete all the rest */
+	/* delete all */
 	lines[1] = NULL;
 
 	curline = 0;
 	assert(read_config() == 0);
 
-	assert(global == NULL);
+	assert(global != NULL); // globals not deleted
 	assert(folderlist == NULL);
-	assert(ignorelist == NULL);
-
-	/* test folderlist */
-	lines[0] = "[folders]";
-	lines[1] = "Review Request=+Reviews";
-	lines[2] = "Confluence] BTS=inbox";
-	lines[3] = "Confluence]=+Confluence";
-	lines[4] = "<jirabbqnx@blackberry.com>=Bugs";
-	lines[5] = NULL;
-
-	curline = 0;
-	assert(read_config() == 0);
-
-	verify_list(folderlist, 1);
-
-	/* delete everything again */
-	lines[1] = NULL;
-
-	curline = 0;
-	assert(read_config() == 0);
-
-	verify_list(folderlist, 1);
+	assert(graylist == NULL);
 
 	puts("Success!");
 	return 0;
@@ -172,6 +132,6 @@ int main(int argc, char *argv[])
 
 /*
  * Local Variables:
- * compile-command: "gcc -I.. -DIMAP -g -coverage -Wall test_config.c -o test_config"
+ * compile-command: "gcc -I.. -DIMAP -g -Wall test_config.c -o test_config"
  * End:
  */
